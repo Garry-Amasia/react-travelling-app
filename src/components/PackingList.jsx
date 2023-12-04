8b591d56ae1768f87ad08c6be940b3cde3bfa108
GarryAmasia <amasiagarry87@gmail.com>
2023-12-04 16:37:46 +1100

    first commit

---
+++ src/components/PackingList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PackingList = () => {
+  return <div className="list">List</div>;
+};
---
+++ src/components/Stats.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Stats = () => {
+  return (
+    <footer className="stats">
+      You have x items on your list and you already packed x (x%)
+    </footer>
+  );
+};
